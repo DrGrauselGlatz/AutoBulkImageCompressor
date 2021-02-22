b07f0fe1a1afceba2055383eae3bfe549277768d
Marc Bössow <marc@boessow.one>
2021-02-22 08:26:29 +0100

    remove exmaples - extend batch

--- opt.bat
+++ opt.bat
@@ -1,5 +1,7 @@
 @echo off 
 
-FOR %%i IN (*.*) DO npx @squoosh/cli --mozjpeg '{"quality":75,"baseline":false,"arithmetic":false,"progressive":true,"optimize_coding":true,"smoothing":0,"color_space":3,"quant_table":3,"trellis_multipass":false,"trellis_opt_zero":false,"trellis_opt_table":false,"trellis_loops":1,"auto_subsample":true,"chroma_subsample":2,"separate_chroma_quality":false,"chroma_quality":75}' %%i
+Set quality=75
+
+FOR %%i IN (*.*) DO if not "%%i" == "opt.bat" npx @squoosh/cli --mozjpeg '{"quality":%quality%,"baseline":false,"arithmetic":false,"progressive":true,"optimize_coding":true,"smoothing":0,"color_space":3,"quant_table":3,"trellis_multipass":false,"trellis_opt_zero":false,"trellis_opt_table":false,"trellis_loops":1,"auto_subsample":true,"chroma_subsample":2,"separate_chroma_quality":false,"chroma_quality":%quality%}' %%i
 
 break
